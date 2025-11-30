67aa0bc7bed6e988d7212504e0facdff5168ca62
Deepanshu Taneja <deepanshutaneja762@gmail.com>
2025-11-30 19:35:55 +0530

    sadf

--- suspense/payments/webhook_handler.py
+++ suspense/payments/webhook_handler.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+from django.conf import settings
+
 
 logger = logging.getLogger(__name__)
 
